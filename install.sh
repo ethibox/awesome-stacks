f7d26dccd5d1974c4a25785fd6fb1abf881a802e
Matthieu FRONTON <m@tthieu.fr>
2020-12-16 08:43:10 +0100

    Protect install.sh against partial download

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+do_install() {
 echo "Executing docker install script..."
 curl -fsSL https://get.docker.com | sh > /dev/null 2>&1
 echo "Initialize docker swarm..."
@@ -8,3 +9,7 @@ echo "Deploy traefik reverse-proxy..."
 docker network create --driver=overlay traefik-net >/dev/null 2>&1
 docker stack deploy -c <(curl -so - https://ethibox.github.io/stacks/traefik.yml) traefik > /dev/null 2>&1
 echo "Installation completed"
+}
+
+# run the script upon successful download
+do_install
