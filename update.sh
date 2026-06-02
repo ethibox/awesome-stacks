7b5547f50aa410aeaf12096989aa2616f7729ede
johackim <contact@johackim.com>
2026-06-02 20:01:05 +0200

    feat: update update.sh

--- update.sh
+++ update.sh
@@ -53,7 +53,7 @@ update_app() {
 }
 
 main() {
-  local apps=(${@:-nextcloud discourse rocketchat n8n mastodon mattermost metabase jitsi kanboard matomo mediawiki grist zammad wordpress nocobase wekan mautic calendso freescout})
+  local apps=(${@:-traefik nextcloud discourse rocketchat n8n mastodon mattermost metabase jitsi kanboard matomo mediawiki grist zammad wordpress nocobase wekan mautic calendso freescout})
   for app in "${apps[@]}"; do
     update_app "$app"
   done
